a86691750fb9551097b155ed3d3609b73ac763d4
Jeremiah Black <jeremiahblackol@gmail.com>
2020-04-21 08:51:31 -0600

    modified these testing files

--- test/manager-test.js
+++ test/manager-test.js
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import { expect } from 'chai';
-import Manager from '../src/mamager';
+import Manager from '../src/manager';
 import Hotel from '../src/hotel';
 
 describe('See if the tests are running', function() {
